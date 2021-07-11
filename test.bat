5a76bf58f00c845fbaa08e40857cc70b70d978a6
Jian Chern, Tan <jctan93@gmail.com>
2021-07-11 17:11:19 +0800

    Completed weebify.bat script development
    - Added additional functionality where the script would also set the default subtitle track after multiplexing
    - Added prompts for the user to select which audio and subtitle tracks to keep. Defaults to the most common ones
    - Added prompt for the user to select which subtitle track to set as default. Defaults to the first track
    
    Signed-off-by: Jian Chern, Tan <jctan93@gmail.com>

--- test.bat
+++
@@ -1,9 +0,0 @@
-mkdir new_folder
-
-FOR %%I in (*.mkv) DO (
-	echo %%I
-	mkvmerge --audio-tracks 2  ^
-	--subtitle-tracks 4 ^
-	-o "new_folder\%%I" "%%I"
-)
-
---
+++ weebify.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo "Creating new_folder!" & echo.
+
+set workspace="new_folder"
+
+mkdir %workspace%
+
+set /p audio_track_to_keep="Enter audio track numbers that you wish to keep (delimited by commas if you wish to keep more than one, defaults to track 2): " ^
+							|| SET "audio_track_to_keep=2" & echo.
+
+set /p sub_track_to_keep="Enter subtitle track numbers that you wish to keep (delimited by commas if you wish to keep more than one, defaults to track 4): " ^
+							|| SET "sub_track_to_keep=4" & echo.
+
+set /p default_sub_track="Enter subtitle track to use as default (defaults to track 1): " ^
+							|| SET "default_sub_track=1" & echo.
+
+FOR %%I in (*.mkv) DO (
+	echo %%I
+	mkvmerge --audio-tracks %audio_track_to_keep%  ^
+	--subtitle-tracks %sub_track_to_keep% ^
+	-o "new_folder\%%I" "%%I"
+)
+
+echo. & echo "Setting default subtitle track next!" & echo.
+
+FOR %%I in (%workspace%/*.mkv) DO (
+	echo %%I
+	mkvpropedit --edit track:s%default_sub_track% --set flag-default=1 "new_folder\%%I"
+)
+
